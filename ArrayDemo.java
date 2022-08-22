0d5f030bac08e81a2ee6f38b5f04dc633dabf247
shaiksameera28 <111629915+shaiksameera28@users.noreply.github.com>
2022-08-22 15:50:49 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,12 @@
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		String[] name = new String[10];
+		name[5] ="hyderabad";
+		name[2] ="tokyo";
+		name[1] ="newyork";
+		System.out.println(name[2]);
+	}
+
+}
