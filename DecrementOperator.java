7816126c62e36be867180d5bdaabee35189b3f48
shaiksameera28 <111629915+shaiksameera28@users.noreply.github.com>
2022-08-26 00:00:18 +0530

    Add files via upload

---
+++ DecrementOperator.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class DecrementOperator {
+
+	public static void main(String[] args) {
+		/*
+		int m = 4;
+		int n = 5;
+		m = n--; // post increment
+		System.out.println(m);
+		System.out.println(n);
+		*/
+		
+		int m = 4;
+		int n = 5;
+		m = --n; // pre increment
+		System.out.println(m);
+		System.out.println(n);
+
+	}
+
+}
