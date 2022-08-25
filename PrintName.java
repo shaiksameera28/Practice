7e028c97288391baa06b1fd06107aacf64baf821
shaiksameera28 <111629915+shaiksameera28@users.noreply.github.com>
2022-08-25 23:52:51 +0530

    Add files via upload

--- PrintName.java
+++ PrintName.java
@@ -1,18 +1,19 @@
 package name;
-import java.util.Scanner;
 
 public class PrintName {
 	
-public static void main(String[] args) {
-	String name ="Shaik Sameera";
-	int count = 0;
-	for(int i=0; i<=name.length(); i++) {
-		for(int j=0; j<=name.length(); j++) {
-			if(name.charAt(i)==name.charAt(j)) {
-				count++;
-			}
-			System.out.println(name.charAt(i)+" : "+count);
-		}
+	public static void main(String[] args) {
+       String name = "shaik sameera";
+       int count = 0;
+       for(int i=0;i<name.length();i++) {
+    	   for(int j=0;j<name.length();j++) {
+    		   if(name.charAt(i)==name.charAt(j) && name.charAt(i)!=' ') {
+    			   count++;
+    		   }
+    	   }
+    	   
+    	   System.out.println(name.charAt(i)+" : "+count);
+    	   count = 0;
+       }
 	}
-}
-}
+   }
