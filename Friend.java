d2998e0362fffaa14610bad69dd1bfe2e22dc2a8
shaiksameera28 <111629915+shaiksameera28@users.noreply.github.com>
2022-08-30 16:50:53 +0530

    Add files via upload

---
+++ Friend.java
@@ -0,0 +1,10 @@
+
+public class Friend {
+   public void toEat() {
+	   System.out.println("Meals");
+	   System.out.println("Chinese Food");
+   }
+   public static void main(String args[]) {
+	   
+   }
+}
